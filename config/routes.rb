61e591dd4c370a744b33fe2fe5e818a33cbd8bc9
rurugao1 <rurugao1@gmail.com>
2021-03-13 07:53:28 +0000

    ナビバーの作成とユーザモデルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+
+  get 'users/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+
+  get '/about', to: 'static_pages#about'
+  get '/signup', to: 'users#new'
 end
